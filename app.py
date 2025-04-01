3e15444526a7db655e1da0c52dd294686800004d
orwee <orweebusiness@gmail.com>
2025-04-01 21:12:50 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Rocky DeFi Assistant",
+    page_icon="🚀",
+    layout="wide"
+)
+
+# Redirigir a la primera página
+st.markdown("# Rocky DeFi Assistant")
+st.write("Navigate to the pages using the sidebar")
